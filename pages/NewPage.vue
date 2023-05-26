ab87306b0ec5c14d9584d8993c60cd6914d23008
XiaoFan <1066580119@qq.com>
2023-05-26 22:36:42 +0800

    init commit

---
+++ pages/NewPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    自动添加路由，命名为大驼峰
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NewPage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-center items-center ">
+    <span>{{hello}}</span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Vue from 'vue'
+import {ref} from "vue";
+
+const hello = ref("Hello world!")
+</script>
