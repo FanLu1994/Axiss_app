6305fce69c091f208c19f004a86d303bb158f6d5
XiaoFan <1066580119@qq.com>
2023-08-27 20:14:00 +0800

    update:提交更新

---
+++ pages/animate/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div ref="dropdown" class="dropdown">
+    <strong class="dropdown-label" @click="show = !show">
+      Click me to open!
+    </strong>
+    <p v-if="show">
+      Lorum ipsum...
+      Lorum ipsum...
+      Lorum ipsum...
+      Lorum ipsum...
+      Lorum ipsum...
+      Lorum ipsum...  Lorum ipsum...  Lorum ipsum...  Lorum ipsum...
+
+      Lorum ipsum...  Lorum ipsum...  Lorum ipsum...  Lorum ipsum...  Lorum ipsum...  Lorum ipsum...
+      Lorum ipsum...
+
+    </p>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue"
+import autoAnimate from "@formkit/auto-animate"
+
+const dropdown = ref() // we need a DOM node
+const show = ref(false)
+
+onMounted(() => {
+  autoAnimate(dropdown.value) // thats it!
+})
+</script>
+<style scoped>
+.dropdown{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #5c6669;
+}
+
+</style>
